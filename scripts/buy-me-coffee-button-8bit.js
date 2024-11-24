dca5f8b43e14f73689937b5932ce70f77a12bc3b
Pandelis Manikas <pmanikas.social@gmail.com>
2024-11-24 16:33:57 +0200

    Update button styles

--- scripts/buy-me-coffee-button-8bit.js
+++ scripts/buy-me-coffee-button-8bit.js
@@ -1,10 +1,13 @@
 document.currentScript.insertAdjacentHTML('beforebegin', `
     <style>
+    @import url('https://fonts.googleapis.com/css2?family=Play:wght@400;700&family=Press+Start+2P&display=swap');
+
     .coffee {
         display: flex;
         padding: 6px 12px;
         background-color: #fd0;
         align-items: center;
+        gap: 1rem;
         color: #000;
         border: none;
         opacity: 0.2;
@@ -12,23 +15,28 @@ document.currentScript.insertAdjacentHTML('beforebegin', `
         font-size: 12px;
         word-spacing: 0;
         letter-spacing: 0;
+        font-family: 'Press Start 2P', cursive;
     }
 
     .coffee::before {
         content: "";
-        background-image: url("/images/coffee-cup-8bit.png");
+        background-image: url("https://pmanikas.github.io/pmanikas/assets/images/coffee-cup-8bit.png");
         width: 20px;
         height: 40px;
         background-size: contain;
         background-repeat: no-repeat;
         flex-shrink: 0;
-        margin-right: var(--base-spacing);
     }
 
     .coffee:hover {
         opacity: 1;
     }
+
+    .coffee-link {
+        text-decoration: none;
+    }
     </style>
-    <a href="https://buymeacoffee.com/pmanikas"><button class="coffee">Buy me a coffee</button></a>
+
+    <a href="https://buymeacoffee.com/pmanikas" target="_blank" class="coffee-link"><button class="coffee">Buy me a coffee</button></a>
 `);
 document.currentScript.remove();
