b2b475f8f4566bcc4bd596ca6a2862c62e24bda8
Pandelis Manikas <pmanikas.social@gmail.com>
2024-11-25 15:40:11 +0200

    Update hover effect

--- scripts/github-button-8bit.js
+++ scripts/github-button-8bit.js
@@ -3,6 +3,7 @@ document.currentScript.insertAdjacentHTML('beforebegin', `
     .pmanikas-github-8bit {
         width: 64px;
         height: 64px;
+        cursor: pointer;
     }
 
     .pmanikas-github-8bit-link {
@@ -11,7 +12,7 @@ document.currentScript.insertAdjacentHTML('beforebegin', `
     </style>
 
     <a href="https://github.com/pmanikas" target="_blank" class="pmanikas-github-8bit-link">
-        <img width="64" height="64" src="https://pmanikas.github.io/pmanikas/assets/images/github-8bit.png" alt="Pantelis Manikas GitHub" class="pmanikas-github-8bit">
+        <img src="https://pmanikas.github.io/pmanikas/assets/images/github-8bit.png" alt="Pantelis Manikas GitHub" class="pmanikas-github-8bit">
     </a>
 `);
 document.currentScript.remove();
